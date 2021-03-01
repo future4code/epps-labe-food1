51c54a5ed76b897a9f21893ee51009d613714ae4
Vivian-Oliveira <costa.vivs@gmail.com>
2021-03-01 17:36:35 +0000

    criamos rotas, pages, coordinator e hooks

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header () {
+    return(
+        <div>
+            <button>Voltar</button>
+        </div>
+    )
+}
---
+++ src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProfilePage () {
+    return(
+        <div>
+            <h1>Pagina de Perfil</h1>
+        </div>
+    )
+}
