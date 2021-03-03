43a15b28c49d870394ff59f0734cf6631ba1afdb
julianapedroso <juliana.cristina@hotmail.com.br>
2021-03-02 22:09:00 -0300

    criação do componente footer com a funcionalidade de ir para as páginas de feed, buycart e profile, criação do loading e início da criação da página buycart

--- src/pages/BuyCart/BuyCart.js
+++ src/pages/BuyCart/BuyCart.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer'
 
 export default function BuyCart () {
     return(
         <div>
             <h1>carrinho</h1>
+            <Footer />
         </div>
     )
 }
