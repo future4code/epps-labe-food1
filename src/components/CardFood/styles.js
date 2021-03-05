84bc91267eee109425956462413ce30b24900644
Fernando <Fernando>
2021-03-05 12:18:45 -0300

    carrinho de compras

---
+++ src/components/CardFood/styles.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+export const OrderCard = styled.div`
+  display: flex;
+  width: 328px;
+  height:17vh;
+  margin: 8px;
+  border-radius: 8px;
+  align-items: center;
+  border: 1px solid #b8b8b8;
+  color: #5cb646;
+`;
+
+export const OrderTitle = styled(Typography)`
+  margin-top: 18px;
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #5cb646;
+`;
+
+export const MainCardContent = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  width: 70%;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+
+  p:first-child {
+    width: 118px;
+    height: 19px;
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: #000;
+  }
+
+  p {
+    width: 200px;
+    height: 30px;
+    margin-top: 5px;
+    font-family: "Roboto", sans-serif;
+    font-size: 12px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+  }
+`;
+
+export const OrderImage = styled.img`
+  width: 96px;
+  height: 112px;
+  margin: 0 16px 0 0;
+  border-radius: 7px 0 0 7px;
+  border: none;
+  /* object-fit: contain; */
+`;
+
+export const OrderPrice = styled.div`
+  display: flex;
+  width: 100%;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+`;
+
+export const OrderButton = styled.button`
+  display: flex;
+  align-self: flex-end;
+  flex-flow: column wrap;
+  width: 90px;
+  height: 31px;
+  margin-top: 8px;
+  padding: 8px 20.5px 8px 21.5px;
+  border-top-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border: 1px solid #5cb646;
+  color: #5cb646;
+  cursor: pointer;
+  background-color: transparent;
+`;
--- src/pages/BuyCart/BuyCart.js
+++ src/pages/BuyCart/BuyCart.js
@@ -1,20 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import AddressDelivery from '../../components/AddressDelivery/AdressDelivery';
 import Footer from '../../components/Footer/Footer'
 import Payment from '../../components/Payment/Payment';
 import useProtectedPage from '../../hooks/useProtectedPage';
-import { getActiveOrder } from '../../Services/use';
 import {P, ContainerCart} from './styles'
+import GlobalStateContext from '../../context/GlobalStateContext';
+import CardFood from '../../components/CardFood/CardFood'
 
 export default function BuyCart () {
     useProtectedPage();
     const [orders, setOrders] = useState()
-    useEffect(()=>{
-        getActiveOrder(setOrders)
-    },[])
+    const { states, requests, setters } = useContext(GlobalStateContext);
+    
+    // Os valores são estáticos e devem ser apagados
+    const cartTeste =[{
+        amount: 1,
+        category: "Pastel",
+        description: "Pastel autêntico, feito na hora!",
+        id: "3vcYYSOEf8dKeTPd7vHe",
+        name: "Pastel",
+        photoUrl: "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031408_66194519.jpg",
+        price: 3,
+    },
+    {
+        amount: 1,
+        category: "Pastel",
+        description: "Pastel autêntico, feito na hora!",
+        id: "3vcYYSOEf8dKeTPd7vHe",
+        name: "Pastel",
+        photoUrl: "https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/65c38aa8-b094-413d-9a80-ddc256bfcc78/201907031408_66194519.jpg",
+        price: 3,
+    }
+]
+
     return(
         <ContainerCart>
             <AddressDelivery/>
+            {/* {states.cart && states.cart.map((food)=>{  Deve ser utilizado para renderizar os dados*/} 
+                {cartTeste.map((food)=>{ // deve ser apagado
+                return <CardFood
+                            amount = {food.amount}
+                            category ={food.category}
+                            description={food.description}
+                            id={food.id}
+                            name={food.name}
+                            photoUrl={food.photoUrl}
+                            price={food.price}
+                />
+            })}
             
             <Payment/>
             
@@ -22,9 +55,3 @@ export default function BuyCart () {
         </ContainerCart>
     )
 }
-
-// order:
-// createdAt: 1614815276070
-// expiresAt: 1614818876070
-// restaurantName: "Habibs"
-// totalPrice: 41
